fb67a0b563c1c20cd27fb9c0d4c1b88967d8ebd6
Irina <irbris@mail.ru>
2020-10-14 20:32:43 +0300

    Add files via upload

---
+++ cross-check/criteria-list.js
@@ -0,0 +1,91 @@
+export const task = "calculator";
+
+export const criteria = [
+  {
+    type: "title",
+    title: ""
+  },
+  {
+    type: 'subtask',
+    text: "",
+    max: 2,
+  },
+  {
+    type: 'subtask',
+    text: "",
+    max: 2,
+  },
+  {
+    type: 'subtask',
+    text: "",
+    max: 2,
+  },
+  {
+    type: 'subtask',
+    text: "",
+    max: 2,
+  },
+  {
+    type: 'subtask',
+    text: "",
+    max: 2,
+  },  
+  {
+    type: "title",
+    title: ""
+  },
+  {
+    type: 'subtask',
+    text: "",
+    max: 2,
+  },
+  {
+    type: 'subtask',
+    text: "",
+    max: 2,
+  },
+  {
+    type: 'subtask',
+    text: "",
+    max: 2,
+  },
+  {
+    type: 'subtask',
+    text: "",
+    max: 2,
+  },
+  {
+    type: 'subtask',
+    text: "",
+    max: 2,
+  }, 
+  {
+    type: "title",
+    title: ""
+  },
+  {
+    type: 'subtask',
+    text: "",
+    max: 2,
+  },
+  {
+    type: 'subtask',
+    text: "",
+    max: 2,
+  },
+  {
+    type: 'subtask',
+    text: "",
+    max: 2,
+  },
+  {
+    type: 'subtask',
+    text: "",
+    max: 2,
+  },
+  {
+    type: 'subtask',
+    text: "",
+    max: 2,
+  },
+]
